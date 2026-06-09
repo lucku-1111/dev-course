3af5bc3796ef186f6bc4989d1f46440d04cebaa2
lucku <lsw6731@naver.com>
2026-06-09 15:16:21 +0900

    과제1 static 제거하기 - static 키워드를 제거해 힙 영역에서 객체가 상태를 제어하기 때문에 더 객체지향적임

--- java-part-1/src/main/java/I_vending_machine.java
+++ java-part-1/src/main/java/I_vending_machine.java
@@ -1,91 +1,59 @@
 import java.util.Scanner;
 
 public class I_vending_machine {
+
     static final int COKE = 500, CIDER = 500, FANTA = 300, WATER = 200;
 
-    public static void printMenu(int totalMoney) {
+    private int totalMoney = 0; // 상태를 객체가 직접 관리
+    private Scanner sc = new Scanner(System.in);
+
+    public void printMenu() { // totalMoney 파라미터 불필요
         System.out.println("============== 자판기 ==============");
         System.out.println("[1]콜라 : 500, [2]사이다 : 500, [3]환타 : 300, [4]물 : 200, [5]돈 넣기, [6]종료");
         System.out.println("현재 금액 : " + totalMoney);
         System.out.println("====================================");
     }
 
-    public static int getChoice() {
-        Scanner sc = new Scanner(System.in);
+    public int getChoice() {
         System.out.println("원하는 메뉴를 선택하시오.");
-
         return sc.nextInt();
     }
 
-    public static int getMoney() {
-        Scanner sc = new Scanner(System.in);
+    public void insertMoney() {
         System.out.println("돈을 넣으시오.");
-
-        return sc.nextInt();
+        totalMoney += sc.nextInt(); // 직접 상태 변경
     }
 
-    public static int calcMoney(int money, int price) {
-        return money - price;
+    public void purchase(int price, String itemName) {
+        int result = totalMoney - price;
+        if (result < 0) {
+            System.out.println("잔돈이 부족합니다.");
+        } else {
+            totalMoney = result;
+            System.out.println(itemName + "가 나왔습니다.");
+        }
     }
 
-    public static void calcMoneyException() {
-        System.out.println("잔돈이 부족합니다.");
-    }
-
-    public static void main(String[] args) {
-        int totalMoney = 0;
-
+    public void run() {
         while (true) {
-            printMenu(totalMoney);
+            printMenu();
             int choice = getChoice();
-            int result = -1;
             switch (choice) {
-                case 1:
-                    result = calcMoney(totalMoney, COKE);
-                    if (result < 0) {
-                        calcMoneyException();
-                    } else {
-                        totalMoney = result;
-                        System.out.println("콜라가 나왔습니다.");
-                    }
-                    break;
-                case 2:
-                    result = calcMoney(totalMoney, CIDER);
-                    if (result < 0) {
-                        calcMoneyException();
-                    } else {
-                        totalMoney = result;
-                        System.out.println("사이다가 나왔습니다.");
-                    }
-                    break;
-                case 3:
-                    result = calcMoney(totalMoney, FANTA);
-                    if (result < 0) {
-                        calcMoneyException();
-                    } else {
-                        totalMoney = result;
-                        System.out.println("환타가 나왔습니다.");
-                    }
-                    break;
-                case 4:
-                    result = calcMoney(totalMoney, WATER);
-                    if (result < 0) {
-                        calcMoneyException();
-                    } else {
-                        totalMoney = result;
-                        System.out.println("물이 나왔습니다.");
-                    }
-                    break;
-                case 5:
-                    totalMoney += getMoney();
-                    break;
+                case 1: purchase(COKE,  "콜라");  break;
+                case 2: purchase(CIDER, "사이다"); break;
+                case 3: purchase(FANTA, "환타");  break;
+                case 4: purchase(WATER, "물");    break;
+                case 5: insertMoney(); break;
                 case 6:
                     System.out.println("잔돈 " + totalMoney + "원이 반환되었습니다.");
                     return;
                 default:
                     System.out.println("잘못된 입력값입니다. 다시 입력해주세요.");
-                    break;
             }
         }
+    }
+
+    public static void main(String[] args) {
+        new I_vending_machine().run(); // 객체 생성 후 실행
     }
 }
--- java-part-1/src/main/java/K_member_management.java
+++ java-part-1/src/main/java/K_member_management.java
@@ -1,35 +1,54 @@
 import java.util.Scanner;
 
+class Member {
+    String name;
+    String email;
+    String phone;
+
+    public Member(String name, String email, String phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "[이름] " + name + ", [메일] " + email + ", [연락처] " + phone;
+    }
+}
+
 public class K_member_management {
 
-    static int totalCnt = 0;
-    static int memberCnt = 0;
+    private Member[] members;
+    private int totalCnt;
+    private int memberCnt = 0;
+    private Scanner sc = new Scanner(System.in);
 
-    public static int printPricePlan() {
-        Scanner sc = new Scanner(System.in);
+    public K_member_management(int totalCnt) {
+        this.totalCnt = totalCnt;
+        this.members = new Member[totalCnt];
+    }
+
+    public int printPricePlan() {
         System.out.println("[요금제를 선택하세요]");
         System.out.println("[1]Lite : 10명 [2]Basic : 20명 [3]Premium : 30명");
-
         return sc.nextInt();
     }
 
-    public static int printMenu() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("[수행할 업무를 선택하세요 - 현재 회원수 : " + memberCnt + "/" + totalCnt  + "]");
+    public int printMenu() {
+        System.out.println("[수행할 업무를 선택하세요 - 현재 회원수 : " + memberCnt + "/" + totalCnt + "]");
         System.out.println("[1]회원추가 [2]회원조회(메일) [3]회원조회(이름)");
         System.out.println("[4]회원전체조회 [5]회원정보 수정 [6]회원삭제");
         System.out.println("[7]프로그램 종료");
-
         return sc.nextInt();
     }
 
-    public static void addMember(String[][] members) {
+    public void addMember() {
         if (memberCnt == totalCnt) {
             System.out.println("회원이 꽉찼습니다.");
             return;
         }
 
-        Scanner sc = new Scanner(System.in);
         System.out.println("이름을 입력하세요.");
         String name = sc.nextLine();
         System.out.println("이메일을 입력하세요.");
@@ -37,164 +56,123 @@ public class K_member_management {
         System.out.println("연락처를 입력하세요.");
         String phone = sc.nextLine();
 
-        if (checkEmail(members, email)) {
+        if (checkEmail(email)) {
             System.out.println("이미 존재하는 회원입니다.");
             return;
         }
 
-        members[memberCnt][0] = name;
-        members[memberCnt][1] = email;
-        members[memberCnt][2] = phone;
-
-        memberCnt++;
+        members[memberCnt++] = new Member(name, email, phone);
     }
 
-    public static boolean checkEmail(String[][] members, String email) {
-        for (int i = 0; i < members.length; i++) {
-            if (email.equals(members[i][1])) {
-                return true;
-            }
+    public boolean checkEmail(String email) {
+        for (int i = 0; i < memberCnt; i++) {
+            if (email.equals(members[i].email)) return true;
         }
         return false;
     }
 
-    public static void selectEmail(String[][] members) {
-        Scanner sc = new Scanner(System.in);
+    public void selectEmail() {
         System.out.println("[조회] 이메일을 입력하세요.");
         String email = sc.nextLine();
 
-        for (int i = 0; i < members.length; i++) {
-            if (email.equals(members[i][1])) {
-                System.out.println("[이름] " + members[i][0] + ", [메일] " + members[i][1] + ", [연락처] " + members[i][2]);
+        for (int i = 0; i < memberCnt; i++) {
+            if (email.equals(members[i].email)) {
+                System.out.println(members[i]);
                 return;
             }
         }
         System.out.println("찾으시는 정보가 없습니다.");
     }
 
-    public static void selectName(String[][] members) {
-        Scanner sc = new Scanner(System.in);
+    public void selectName() {
         System.out.println("[조회] 이름을 입력하세요.");
         String name = sc.nextLine();
 
-        for (int i = 0; i < members.length; i++) {
-            if (name.equals(members[i][0])) {
-                System.out.println("[이름] " + members[i][0] + ", [메일] " + members[i][1] + ", [연락처] " + members[i][2]);
+        for (int i = 0; i < memberCnt; i++) {
+            if (name.equals(members[i].name)) {
+                System.out.println(members[i]);
                 return;
             }
         }
         System.out.println("찾으시는 정보가 없습니다.");
     }
 
-    public static void selectAll(String[][] members) {
-        for (int i = 0; i < members.length; i++) {
-            System.out.println("[이름] " + members[i][0] + ", [메일] " + members[i][1] + ", [연락처] " + members[i][2]);
+    public void selectAll() {
+        if (memberCnt == 0) {
+            System.out.println("등록된 회원이 없습니다.");
+            return;
+        }
+        for (int i = 0; i < memberCnt; i++) {
+            System.out.println(members[i]);
         }
     }
 
-    public static void updateMember(String[][] members) {
-        Scanner sc = new Scanner(System.in);
+    public void updateMember() {
         System.out.println("[수정] 이메일을 입력하세요.");
         String email = sc.nextLine();
-        int idx = -1;
-        for (int i = 0; i < members.length; i++) {
-            if (email.equals(members[i][1])) {
-                idx = i;
-                break;
+
+        for (int i = 0; i < memberCnt; i++) {
+            if (email.equals(members[i].email)) {
+                System.out.println("변경할 이름을 입력하세요.");
+                members[i].name = sc.nextLine();
+                System.out.println("변경할 이메일을 입력하세요.");
+                members[i].email = sc.nextLine();
+                System.out.println("변경할 연락처를 입력하세요.");
+                members[i].phone = sc.nextLine();
+                System.out.println("수정이 완료되었습니다.");
+                return;
             }
         }
-
-        if ( idx == -1 ) {
-            System.out.println("찾으시는 회원이 없습니다.");
-            return;
-        }
-
-        System.out.println("변경할 이름을 입력하세요.");
-        String newName = sc.nextLine();
-        System.out.println("변경할 이메일을 입력하세요.");
-        String newEmail = sc.nextLine();
-        System.out.println("변경할 연락처를 입력하세요.");
-        String newPhone = sc.nextLine();
-
-        members[idx][0] = newName;
-        members[idx][1] = newEmail;
-        members[idx][2] = newPhone;
-
-        System.out.println("수정이 완료되었습니다.");
+        System.out.println("찾으시는 회원이 없습니다.");
     }
 
-    public static void deleteMember(String[][] members) {
-        Scanner sc = new Scanner(System.in);
+    public void deleteMember() {
         System.out.println("[삭제] 이메일을 입력하세요.");
         String email = sc.nextLine();
-        int idx = -1;
 
-        for (int i = 0; i < members.length; i++) {
-            if ( email.equals(members[i][1]) ) {
-                idx = i;
-                break;
+        for (int i = 0; i < memberCnt; i++) {
+            if (email.equals(members[i].email)) {
+                for (int j = i; j < memberCnt - 1; j++) {
+                    members[j] = members[j + 1];
+                }
+                members[--memberCnt] = null;
+                System.out.println("삭제가 완료되었습니다.");
+                return;
             }
         }
-
-        if ( idx == -1 ) {
-            System.out.println("찾으시는 회원이 없습니다.");
-            return;
-        }
-
-        for (int i = idx; i < memberCnt-1; i++) {
-            members[i][0] = members[i + 1][0];
-            members[i][1] = members[i + 1][1];
-            members[i][2] = members[i + 1][2];
-        }
-
-        members[memberCnt-1][0] = null;
-        members[memberCnt-1][1] = null;
-        members[memberCnt-1][2] = null;
-
-        memberCnt--;
+        System.out.println("찾으시는 회원이 없습니다.");
     }
 
-    public static void main(String[] args) {
-
+    public void run() {
         int pricePlan = printPricePlan();
 
-        if (pricePlan > 3 || pricePlan < 1) {
-            System.out.println("잘못된 입력값입니다. [1 ~ 3의 숫자를 입력해주세요.");
+        if (pricePlan < 1 || pricePlan > 3) {
+            System.out.println("잘못된 입력값입니다. [1 ~ 3의 숫자를 입력해주세요.]");
             return;
         }
 
-        totalCnt = pricePlan * 10;
-        String[][] members = new String[totalCnt][3];
-        System.out.println(totalCnt);
+        this.totalCnt = pricePlan * 10;
+        this.members = new Member[totalCnt];
+
         while (true) {
             int menu = printMenu();
-            System.out.println(menu);
             switch (menu) {
-                case 1:
-                    addMember(members);
-                    break;
-                case 2:
-                    selectEmail(members);
-                    break;
-                case 3:
-                    selectName(members);
-                    break;
-                case 4:
-                    selectAll(members);
-                    break;
-                case 5:
-                    updateMember(members);
-                    break;
-                case 6:
-                    deleteMember(members);
-                    break;
+                case 1: addMember();   break;
+                case 2: selectEmail(); break;
+                case 3: selectName();  break;
+                case 4: selectAll();   break;
+                case 5: updateMember(); break;
+                case 6: deleteMember(); break;
                 case 7:
                     System.out.println("이용해주셔서 감사합니다.");
                     return;
                 default:
                     System.out.println("잘못된 입력값입니다.");
-                    break;
             }
         }
+    }
+
+    public static void main(String[] args) {
+        new K_member_management(0).run();
     }
 }
