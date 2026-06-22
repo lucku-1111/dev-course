327abc56ebc080437a5d1b3a95141b1b04be585b
lucku <lsw6731@naver.com>
2026-06-22 11:49:15 +0900

    260622 spring-theory ex_1_3

---
+++ spring-theory/src/main/java/com/example/spring/springtheory/ch01/ex_1_3/dao/SimpleConnectionMaker.java
@@ -0,0 +1,9 @@
+package com.example.spring.springtheory.ch01.ex_1_3.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface SimpleConnectionMaker {
+
+    Connection makeNewConnection() throws ClassNotFoundException, SQLException;
+}
