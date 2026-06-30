b103cce07ea174a46350696b898c48decb2857be
lucku <lsw6731@naver.com>
2026-06-30 10:21:54 +0900

    260630 spring-theory ex_5_3

---
+++ spring-theory/src/main/java/com/example/spring/springtheory/ch05/ex_5_3/service/UserServiceTx.java
@@ -0,0 +1,37 @@
+package com.example.spring.springtheory.ch05.ex_5_3.service;
+
+import com.example.spring.springtheory.ch05.ex_5_3.domain.User;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import java.sql.SQLException;
+
+public class UserServiceTx implements UserService {
+
+    private PlatformTransactionManager transactionManager;
+    private UserServiceImpl userService;
+
+    public UserServiceTx(PlatformTransactionManager transactionManager, UserServiceImpl userService) {
+        this.transactionManager = transactionManager;
+        this.userService = userService;
+    }
+
+    @Override
+    public void add(User user) throws SQLException, ClassNotFoundException {
+        userService.add(user);
+    }
+
+    @Override
+    public void upgradeLevels() {
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+        try {
+            userService.upgradeLevels();
+            transactionManager.commit(status);
+        } catch (Exception e) {
+            transactionManager.rollback(status);
+            throw e;
+        }
+    }
+}
