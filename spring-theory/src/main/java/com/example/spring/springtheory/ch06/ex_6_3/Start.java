ae7b28dbb2073999bcb0561f2e25ba2f549f61b6
lucku <lsw6731@naver.com>
2026-07-01 10:44:20 +0900

    260701 spring theory ch06

---
+++ spring-theory/src/main/java/com/example/spring/springtheory/ch06/ex_6_3/Start.java
@@ -0,0 +1,14 @@
+package com.example.spring.springtheory.ch06.ex_6_3;
+
+// * 스프링 AOP
+// * 문제점
+// ex_6_2는 부가기능을 적용할 빈마다 ProxyFactoryBean을 '하나씩' 설정해야 한다.
+// (target 지정 + advisor 등록을 빈 개수만큼 반복)
+
+// '자동 프록시 생성기'가 프록시 적용을 자동화 한다.
+
+public class Start {
+    static void main() {
+
+    }
+}
