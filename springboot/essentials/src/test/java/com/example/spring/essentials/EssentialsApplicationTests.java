95d835580040783f174c4fb72afb5b4f42d9f496
lucku <lsw6731@naver.com>
2026-07-02 10:32:35 +0900

    260702 spring boot essentials

---
+++ springboot/essentials/src/test/java/com/example/spring/essentials/EssentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.essentials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EssentialsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
