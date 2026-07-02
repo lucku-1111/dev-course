e611bb35e7da2723c8a9dc6c21c60d62943acd5e
lucku <lsw6731@naver.com>
2026-07-02 15:33:03 +0900

    260702 과제2 스프링부트 1.세션 쿠키

---
+++ 과제/sessioncookie/src/main/java/com/example/spring/sessioncookie/SessioncookieApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.sessioncookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessioncookieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SessioncookieApplication.class, args);
+    }
+
+}
