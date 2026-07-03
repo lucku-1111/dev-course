3ecd2058b8d9557c314774ba360b090f4e0425b4
lucku <lsw6731@naver.com>
2026-07-03 10:35:40 +0900

    260703 springboot feign client

--- springboot/feign/feign-api/src/main/java/com/example/spring/feignapi/controller/DataController.java
+++ springboot/feign/feign-api/src/main/java/com/example/spring/feignapi/controller/DataController.java
@@ -3,10 +3,7 @@ package com.example.spring.feignapi.controller;
 import com.example.spring.feignapi.dto.DataRequest;
 import com.example.spring.feignapi.dto.DataResponse;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -27,7 +24,9 @@ public class DataController {
     }
 
     @GetMapping("/{id}")
-    public DataResponse getDataById(Long id) {
+    public DataResponse getDataById(
+            @PathVariable Long id
+    ) {
         DataResponse dataResponse = dataStore.get(id);
 
         if (dataResponse == null) throw new RuntimeException("Data not found: " + id);
@@ -48,5 +47,32 @@ public class DataController {
         dataStore.put(build.getId(), build);
 
         return build;
+    }
+
+    @PutMapping("/{id}")
+    public DataResponse updateData(
+            @PathVariable("id") Long id, // "id"는 매개변수 값이 id로 경로 변수값과 이름이 일치해서 넣지 않아도 되지만 학습용으로 넣음
+            @RequestBody DataRequest dataRequest
+    ) {
+        DataResponse dataResponse = dataStore.get(id);
+
+        if (dataResponse == null) throw new RuntimeException("Data not found(update): " + id);
+
+        dataResponse.setName(dataResponse.getName());
+        dataResponse.setValue(dataResponse.getValue());
+        dataStore.put(id, dataResponse);
+
+        return dataResponse;
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteData(
+            @PathVariable Long id
+    ) {
+        DataResponse removed = dataStore.remove(id);
+
+        if (removed == null) throw new RuntimeException("Data not found(delete): " + id);
+
+        return "Data deleted with id:" + id;
     }
 }
