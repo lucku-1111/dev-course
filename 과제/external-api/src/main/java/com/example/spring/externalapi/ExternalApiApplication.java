5d4783377b631eb31cb37be51dc2ae000d520756
lucku <lsw6731@naver.com>
2026-07-03 16:48:01 +0900

    260703 과제2 스프링부트 2. 외부 API 호출

---
+++ 과제/external-api/src/main/java/com/example/spring/externalapi/ExternalApiApplication.java
@@ -0,0 +1,15 @@
+package com.example.spring.externalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class ExternalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExternalApiApplication.class, args);
+    }
+
+}
