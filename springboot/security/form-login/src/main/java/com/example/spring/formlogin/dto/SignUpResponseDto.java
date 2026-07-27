c4071adb3d955a6317d96b50995f6182c2bdbf6a
lucku <lsw6731@naver.com>
2026-07-27 11:34:00 +0900

    260727 springboot security form login

---
+++ springboot/security/form-login/src/main/java/com/example/spring/formlogin/dto/SignUpResponseDto.java
@@ -0,0 +1,10 @@
+package com.example.spring.formlogin.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SignUpResponseDto {
+    private String url;
+}
