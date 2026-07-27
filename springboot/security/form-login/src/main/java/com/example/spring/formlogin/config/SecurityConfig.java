a37814ae54bde2fce760044727763d84afbf328f
lucku <lsw6731@naver.com>
2026-07-27 12:05:56 +0900

    260727 springboot security form login

--- springboot/security/form-login/src/main/java/com/example/spring/formlogin/config/SecurityConfig.java
+++ springboot/security/form-login/src/main/java/com/example/spring/formlogin/config/SecurityConfig.java
@@ -44,7 +44,12 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/css/**", "/js/**").permitAll()
+                        .requestMatchers(
+                                "/users/join",
+                                "/api/users/join",
+                                "/css/**",
+                                "/js/**")
+                        .permitAll()
                         .anyRequest().authenticated()
                 );
 
