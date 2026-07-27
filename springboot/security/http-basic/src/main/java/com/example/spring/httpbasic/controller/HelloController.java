40dd69bd35f4e5af7d434f769874a8ae031b741e
lucku <lsw6731@naver.com>
2026-07-27 10:41:08 +0900

    260727 springboot security http basic

---
+++ springboot/security/http-basic/src/main/java/com/example/spring/httpbasic/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.spring.httpbasic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+
+}
