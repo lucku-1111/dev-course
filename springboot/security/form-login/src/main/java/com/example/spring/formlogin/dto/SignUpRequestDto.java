3c109d48f2f9dd6bbbca0acd5713479d16af03b1
lucku <lsw6731@naver.com>
2026-07-27 11:42:17 +0900

    260727 springboot security form login

--- springboot/security/form-login/src/main/java/com/example/spring/formlogin/dto/SignUpRequestDto.java
+++ springboot/security/form-login/src/main/java/com/example/spring/formlogin/dto/SignUpRequestDto.java
@@ -1,5 +1,6 @@
 package com.example.spring.formlogin.dto;
 
+import com.example.spring.formlogin.domain.entity.User;
 import lombok.Getter;
 
 @Getter
@@ -8,5 +9,13 @@ public class SignUpRequestDto {
     private String userId;
     private String password;
     private String userName;
+
+    public User toUser(String encodedPassword) {
+        return User.builder()
+                .userId(userId)
+                .password(encodedPassword)
+                .name(userName)
+                .build();
+    }
 
 }
--- springboot/security/form-login/src/main/java/com/example/spring/formlogin/service/UserService.java
+++ springboot/security/form-login/src/main/java/com/example/spring/formlogin/service/UserService.java
@@ -1,6 +1,11 @@
 package com.example.spring.formlogin.service;
 
+import com.example.spring.formlogin.domain.entity.User;
+import com.example.spring.formlogin.domain.repository.UserRepository;
+import com.example.spring.formlogin.dto.SignUpRequestDto;
+import com.example.spring.formlogin.exception.DuplicateUserIdException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,6 +14,19 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class UserService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
+    @Transactional
+    public void signUp(SignUpRequestDto signUpRequestDto) {
+
+        if (userRepository.existsByUserId(signUpRequestDto.getUserId())) {
+            throw new DuplicateUserIdException("[회원가입] 이미 사용 중인 아이디입니다.");
+        }
+
+        User user = signUpRequestDto.toUser(passwordEncoder.encode(signUpRequestDto.getPassword()));
+
+        userRepository.save(user);
+    }
 
 }
