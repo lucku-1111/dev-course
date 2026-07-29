c4e32851da4bd7c68c4a56aa431a0e6518e74d89
lucku <lsw6731@naver.com>
2026-07-29 14:42:01 +0900

    270729 과제2 form login

---
+++ 과제/form-login/src/main/java/com/example/spring/formlogin/controller/UserApiController.java
@@ -0,0 +1,24 @@
+package com.example.spring.formlogin.controller;
+
+import com.example.spring.formlogin.dto.SignUpRequestDto;
+import com.example.spring.formlogin.dto.SignUpResponseDto;
+import com.example.spring.formlogin.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserApiController {
+
+    private final UserService userService;
+
+    @PostMapping("/join")
+    public SignUpResponseDto signUp(@RequestBody SignUpRequestDto request) {
+        userService.signUp(request);
+        return new SignUpResponseDto("/users/login");
+    }
+}
