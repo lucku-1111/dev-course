25e755f7fab59e6f022af2b68f02c1ed0680cd62
lucku <lsw6731@naver.com>
2026-07-31 10:14:42 +0900

    270731 실습 - 게시판 + 토큰(인증, 인가)

---
+++ springboot/board/board-with-token/src/main/java/com/example/spring/boardwithtoken/BoardWithTokenApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.boardwithtoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardWithTokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardWithTokenApplication.class, args);
+    }
+
+}
